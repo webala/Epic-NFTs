5fd35b38458add12a1b5cdf368f912d51865d5c8
webala <webala1001@gmail.com>
2022-05-08 22:12:00 +0300

    initial commit

---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT')
+    const nftContract = await nftContractFactory.deploy()
+    await nftContract.deployed()
+    console.log('Contract deployed to: ', nftContract.address)
+}
+
+const runMaain = async () =>{
+    try {
+        await main()
+        process.exit(0)
+    } catch(error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+runMaain()
