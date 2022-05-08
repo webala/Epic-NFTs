94567e3ce07babb55c054df33b9a3f6f542f854b
webala <webala1001@gmail.com>
2022-05-08 23:40:38 +0300

    deploying nft contract and creating nfts in rinkeby testnet

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT')
+    const nftContract = await nftContractFactory.deploy()
+    await nftContract.deployed()
+    console.log('Contract deployed to: ', nftContract.address)
+
+    let txn = await nftContract.makeAnEpicNFT()
+    //wait for it to be mined
+    await txn.wait()
+    console.log("Minted NFT #1")
+
+    //Mint another NFT for fun
+    txn = await nftContract.makeAnEpicNFT()
+    await txn.wait()
+    console.log("Minted NFT #2")
+}
+
+const runMaain = async () =>{
+    try {
+        await main()
+        process.exit(0)
+    } catch(error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+runMaain()
